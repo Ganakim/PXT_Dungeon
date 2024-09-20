a2dd948ebd84acbdc18fc2084f3f0498a13af1b5
Ganakim <espene.cornwall@gmail.com>
2024-09-20 12:13:13 -0600

    test this

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,14 +1,17 @@
 // Auto-generated code. Do not edit.
 namespace myTiles {
-    //% fixedInstance jres blockIdentity=images._tile
-    export const transparency16 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile1 = image.ofBuffer(hex``);
+  //% fixedInstance jres blockIdentity=images._tile
+  export const transparency16 = image.ofBuffer(hex``);
+  //% fixedInstance jres blockIdentity=images._tile
+  export const tile1 = image.ofBuffer(hex``);
 
-    helpers._registerFactory("tilemap", function(name: string) {
-        switch(helpers.stringTrim(name)) {
-            case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+  helpers._registerFactory("tilemap", function (name: string) {
+    switch (helpers.stringTrim(name)) {
+      case "level1":
+      case "level1":
+        return tiles.createTilemap(
+          hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`,
+          img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -25,9 +28,15 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16], TileScale.Sixteen);
-            case "level2":
-            case "level2":return tiles.createTilemap(hex`1000100016180d160d040f09090b0101010203020d170d0d17040709090a0103020202020d160d0d0d0407090a02020202020201180d0d170d15070b01010202010302011011141310060f0b0102020202020201080509120e08050b010202020202010109090909090c0c0a01020202030201010c0c0c0c0a02020203020202020201010101020202020202020202020202010101020202020302020202020102020101020203030202020302020102010202010302010202020202020203010202020202010101020202010101030202020201020202010202020101010202020202010302020101020202020202031902010102020302020203020203020202010101`, img`
+`,
+          [myTiles.transparency16],
+          TileScale.Sixteen
+        );
+      case "level2":
+      case "level2":
+        return tiles.createTilemap(
+          hex`1000100016180d160d040f09090b0101010203020d170d0d17040709090a0103020202020d160d0d0d0407090a02020202020201180d0d170d15070b01010202010302011011141310060f0b0102020202020201080509120e08050b010202020202010109090909090c0c0a01020202030201010c0c0c0c0a02020203020202020201010101020202020202020202020202010101020202020302020202020102020101020203030202020302020102010202010302010202020202020203010202020202010101020202010101030202020201020202010202020101010202020202010302020101020202020202031902010102020302020203020203020202010101`,
+          img`
 . . . . . 2 2 . . . . . . . . . 
 . . . . . 2 2 . . . . . . . . . 
 . . . . . 2 2 . . . . . . . . . 
@@ -44,19 +53,50 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterNorth0,sprites.castle.tilePath5,sprites.castle.tilePath9,sprites.castle.tilePath6,sprites.castle.tilePath8,sprites.dungeon.floorLight0,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.stairNorth,sprites.dungeon.greenOuterEast1,sprites.dungeon.floorLight4,sprites.dungeon.floorLightMoss,sprites.dungeon.floorLight1,myTiles.tile1], TileScale.Sixteen);
-        }
-        return null;
-    })
+`,
+          [
+            myTiles.transparency16,
+            sprites.castle.tileGrass2,
+            sprites.castle.tileGrass1,
+            sprites.castle.tileGrass3,
+            sprites.dungeon.greenOuterEast0,
+            sprites.dungeon.greenInnerSouthEast,
+            sprites.dungeon.greenOuterSouthWest,
+            sprites.dungeon.greenOuterWest0,
+            sprites.dungeon.greenOuterNorth0,
+            sprites.castle.tilePath5,
+            sprites.castle.tilePath9,
+            sprites.castle.tilePath6,
+            sprites.castle.tilePath8,
+            sprites.dungeon.floorLight0,
+            sprites.dungeon.greenOuterNorth1,
+            sprites.dungeon.greenOuterWest1,
+            sprites.dungeon.greenOuterSouth2,
+            sprites.dungeon.greenInnerNorthEast,
+            sprites.dungeon.greenInnerSouthWest,
+            sprites.dungeon.greenInnerNorthWest,
+            sprites.dungeon.stairNorth,
+            sprites.dungeon.greenOuterEast1,
+            sprites.dungeon.floorLight4,
+            sprites.dungeon.floorLightMoss,
+            sprites.dungeon.floorLight1,
+            myTiles.tile1,
+          ],
+          TileScale.Sixteen
+        );
+    }
+    return null;
+  });
 
-    helpers._registerFactory("tile", function(name: string) {
-        switch(helpers.stringTrim(name)) {
-            case "transparency16":return transparency16;
-            case "myTile":
-            case "tile1":return tile1;
-        }
-        return null;
-    })
-
+  helpers._registerFactory("tile", function (name: string) {
+    switch (helpers.stringTrim(name)) {
+      case "transparency16":
+        return transparency16;
+      case "myTile":
+      case "tile1":
+        return tile1;
+    }
+    return null;
+  });
 }
 // Auto-generated code. Do not edit.
