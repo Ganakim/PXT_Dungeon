e10a3e8057e48da2afc3e03932bad95481d519ec
Spencer C Cornwall <espene.cornwall@gmail.com>
2024-10-04 21:33:34 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,23 @@
 namespace SpriteKind {
     export const Player_Attack = SpriteKind.create()
+    export const Player_Character = SpriteKind.create()
+    export const Dungeon_Worker = SpriteKind.create()
 }
-function currentAnimation () {
-    for (let animation2 of animations) {
-        if (animation2[currentDir()].indexOf(princess.image) != -1) {
-            return animations.indexOf(animation2)
+namespace MultiplayerState {
+    export const animation = MultiplayerState.create()
+    export const x = MultiplayerState.create()
+    export const y = MultiplayerState.create()
+    export const moving = MultiplayerState.create()
+}
+function currentAnimation (playerId: number) {
+    if (mp.getPlayerSprite(mp.getPlayerByIndex(playerId)) != runtimeVariables.undefinedValue()) {
+        for (let a of animations[playerId]) {
+            if (a[currentDir(playerId)].indexOf(mp.getPlayerSprite(mp.getPlayerByIndex(playerId)).image) != -1) {
+                return animations[playerId].indexOf(a)
+            }
         }
     }
-    return -1
+    return 0
 }
 function dirFromXY (x: number, y: number) {
     if (x != 0) {
@@ -15,15 +25,36 @@ function dirFromXY (x: number, y: number) {
     } else if (y != 0) {
         return y + 2
     }
-    console.log("This is a catastrophe")
     return -1
 }
-controller.A.onEvent(ControllerButtonEvent.Released, function () {
-    sprites.setDataNumber(princess, "animation", 0)
+mp.onButtonEvent(mp.MultiplayerButton.A, ControllerButtonEvent.Released, function (p) {
+    mp.setPlayerState(p, MultiplayerState.animation, 0)
 })
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    sprites.setDataNumber(princess, "animation", 2)
-})
+function generateRoom (dWidth: number, dHeight: number) {
+    worker = sprites.create(img`
+        f 
+        `, SpriteKind.Dungeon_Worker)
+    worker.sx = rng.randomRange(3, 8)
+    worker.sy = rng.randomRange(3, 8)
+    worker.x = rng.randomRange(Math.ceil(worker.sx / 2), dWidth - Math.ceil(worker.sx / 2))
+    if (worker.sx % 2 != 0) {
+        worker.x += 0.5
+    }
+    worker.y = rng.randomRange(Math.ceil(worker.sy / 2), dHeight - Math.ceil(worker.sy / 2))
+    if (worker.sy % 2 != 0) {
+        worker.y += 0.5
+    }
+    for (let room of sprites.allOfKind(SpriteKind.Dungeon_Worker)) {
+        if (worker.overlapsWith(room)) {
+            sprites.destroy(worker)
+        }
+    }
+    for (let index = 0; index < worker.sy; index++) {
+        for (let index = 0; index < worker.sx; index++) {
+            tiles.setTileAt(tiles.getTileLocation(0, 0), assets.tile`transparency16`)
+        }
+    }
+}
 function xFromDir (dir: number) {
     if (dir == 0) {
         return -1
@@ -36,170 +67,18 @@ function xFromDir (dir: number) {
     }
     return 0
 }
-function loadLevel (x: number, y: number) {
-    tiles.setCurrentTilemap(map[x][y][0])
-    sprites.setDataNumber(princess, "map.x", x)
-    sprites.setDataNumber(princess, "map.y", y)
-    tiles.placeOnRandomTile(princess, assets.tile`myTile`)
-    spawn = princess.tilemapLocation()
-    tiles.setCurrentTilemap(map[x][y][1])
-}
-function dx () {
-    if (controller.B.isPressed()) {
-        return 0
-    }
-    return Math.min(Math.max(-1, Math.round(controller.dx())), 1)
-}
-function dy () {
-    if (dx() != 0 || controller.B.isPressed()) {
-        return 0
-    }
-    return Math.min(Math.max(-1, Math.round(controller.dy())), 1)
-}
-function currentDir () {
-    return dirFromXY(sprites.readDataNumber(princess, "x"), sprites.readDataNumber(princess, "y"))
-}
-function yFromDir (dir: number) {
-    if (dir == 0) {
-        return 0
-    } else if (dir == 1) {
-        return -1
-    } else if (dir == 2) {
-        return 0
-    } else if (dir == 3) {
-        return 1
-    }
-    return 0
-}
-function snapToGrid (sprite: Sprite) {
-    sprite.x = Math.round((sprite.x - 8) / 16) * 16 + 8
-    sprite.y = Math.round((sprite.y - 8) / 16) * 16 + 8
-}
-let attack: Sprite = null
-let spawn: tiles.Location = null
-let princess: Sprite = null
-let animations: Image[][][] = []
-let map: tiles.TileMapData[][][] = []
-map = [[
-[],
-[],
-[],
-[tilemap`spawbs02`, tilemap`ground02`],
-[]
-], [
-[],
-[],
-[],
-[tilemap`spawns12`, tilemap`ground12`],
-[]
-], [
-[],
-[],
-[],
-[],
-[]
-]]
-animations = [[
-assets.animation`idleLeft`,
-assets.animation`idleUp`,
-assets.animation`idleRight`,
-assets.animation`idleDown`
-], [
-assets.animation`walkLeft`,
-assets.animation`walkUp`,
-assets.animation`walkRight`,
-assets.animation`walkDown`
-], [
-assets.animation`attackLeft`,
-assets.animation`attackUp`,
-assets.animation`attackRight`,
-assets.animation`attackDown`
-]]
-princess = sprites.create(img`
-    . . . . . f f 4 4 f f . . . . . 
-    . . . . f 5 4 5 5 4 5 f . . . . 
-    . . . f e 4 5 5 5 5 4 e f . 9 . 
-    . . f b 3 e 4 4 4 4 e 3 b 9 1 9 
-    . . f 3 3 3 3 3 3 3 3 3 3 f 9 . 
-    . f 3 3 e b 3 e e 3 b e 3 3 f . 
-    . f 3 3 f f e e e e f f 3 3 f . 
-    . f b b f b f e e f b f b b f . 
-    . f b b e 1 f 4 4 f 1 e b b f . 
-    f f b b f 4 4 4 4 4 4 f b b f f 
-    f b b f f f e e e e f f f b b f 
-    . f e e f b d d d d b f e e f . 
-    . . e 4 c d d d d d d c 4 e . . 
-    . . e f b d b d b d b b f e . . 
-    . . . f f 1 d 1 d 1 d f f . . . 
-    . . . . . f f b b f f . . . . . 
-    `, SpriteKind.Player)
-sprites.setDataNumber(princess, "animation", 0)
-sprites.setDataNumber(princess, "x", 0)
-sprites.setDataNumber(princess, "y", 1)
-scene.cameraFollowSprite(princess)
-loadLevel(1, 3)
-tileUtil.setTileAt(map[sprites.readDataNumber(princess, "map.x")][sprites.readDataNumber(princess, "map.y")][0], spawn, assets.tile`transparency16`)
-game.onUpdate(function () {
-    if (sprites.readDataBoolean(princess, "moving")) {
-        if (princess.x - scene.cameraProperty(CameraProperty.X) == -72 && sprites.readDataNumber(princess, "x") == -1) {
-            if (map[sprites.readDataNumber(princess, "map.x") - 1][sprites.readDataNumber(princess, "map.y")].length > 0) {
-                tiles.placeOnTile(princess, tiles.getTileLocation(tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.Columns) - 1, princess.tilemapLocation().row))
-                loadLevel(sprites.readDataNumber(princess, "map.x") - 1, sprites.readDataNumber(princess, "map.y"))
-            }
-        } else if (princess.x - scene.cameraProperty(CameraProperty.X) == 72 && sprites.readDataNumber(princess, "x") == 1) {
-            if (map[sprites.readDataNumber(princess, "map.x") + 1][sprites.readDataNumber(princess, "map.y")].length > 0) {
-                tiles.placeOnTile(princess, tiles.getTileLocation(0, princess.tilemapLocation().row))
-                loadLevel(sprites.readDataNumber(princess, "map.x") + 1, sprites.readDataNumber(princess, "map.y"))
-            }
-        } else if (princess.y - scene.cameraProperty(CameraProperty.Y) == 52 && sprites.readDataNumber(princess, "y") == 1) {
-            if (map[sprites.readDataNumber(princess, "map.x")][sprites.readDataNumber(princess, "map.y") + 1].length > 0) {
-                tiles.placeOnTile(princess, tiles.getTileLocation(princess.tilemapLocation().column, 0))
-                loadLevel(sprites.readDataNumber(princess, "map.x"), sprites.readDataNumber(princess, "map.y") + 1)
-            }
-        } else if (princess.y - scene.cameraProperty(CameraProperty.Y) == -52 && sprites.readDataNumber(princess, "y") == -1) {
-            if (map[sprites.readDataNumber(princess, "map.x")][sprites.readDataNumber(princess, "map.y") - 1].length > 0) {
-                tiles.placeOnTile(princess, tiles.getTileLocation(princess.tilemapLocation().column, tileUtil.tilemapProperty(tileUtil.currentTilemap(), tileUtil.TilemapProperty.Rows) - 1))
-                loadLevel(sprites.readDataNumber(princess, "map.x"), sprites.readDataNumber(princess, "map.y") - 1)
-            }
-        }
-    }
+mp.onControllerEvent(ControllerEvent.Connected, function (p) {
+    mp.setPlayerState(p, 100, 0)
+    mp.setPlayerSprite(p, sprites.create(animations[pid][0][3][0], SpriteKind.Player_Character))
+    mp.setPlayerState(p, MultiplayerState.animation, 0)
+    mp.setPlayerState(p, MultiplayerState.moving, 0)
+    mp.setPlayerState(p, MultiplayerState.x, 0)
+    mp.setPlayerState(p, MultiplayerState.y, 1)
+    pid += 1
 })
-forever(function () {
-    if (!(sprites.readDataBoolean(princess, "moving"))) {
-        if (dx() == sprites.readDataNumber(princess, "x") && dy() == sprites.readDataNumber(princess, "y")) {
-            sprites.setDataBoolean(princess, "moving", true)
-            sprites.setDataNumber(princess, "animation", 1)
-            sprites.setDataNumber(princess, "x", dx())
-            sprites.setDataNumber(princess, "y", dy())
-            princess.setVelocity(dx() * 50, dy() * 50)
-            pause(300)
-            sprites.setDataBoolean(princess, "moving", false)
-            princess.setVelocity(0, 0)
-            snapToGrid(princess)
-        } else if (dx() != 0 || dy() != 0) {
-            sprites.setDataNumber(princess, "x", dx())
-            sprites.setDataNumber(princess, "y", dy())
-            pause(60)
-        } else {
-            if (sprites.readDataNumber(princess, "animation") == 1) {
-                sprites.setDataNumber(princess, "animation", 0)
-            }
-        }
-    }
-})
-game.onUpdate(function () {
-    if (currentAnimation() != sprites.readDataNumber(princess, "animation")) {
-        animation.runImageAnimation(
-        princess,
-        animations[sprites.readDataNumber(princess, "animation")][currentDir()],
-        150,
-        true
-        )
-    }
-})
-forever(function () {
-    if (currentAnimation() == 2 && !(sprites.readDataBoolean(princess, "moving"))) {
-        attack = sprites.create(img`
+function attack (playerId: number, attack_animation: any[]) {
+    if (currentAnimation(playerId) == 2 && mp.getPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.moving) == 0) {
+        attack_effect = sprites.create(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
@@ -217,13 +96,120 @@ forever(function () {
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
             `, SpriteKind.Player_Attack)
-        attack.setPosition(princess.x, princess.y)
-        attack.x += sprites.readDataNumber(princess, "x") * 16
-        attack.y += sprites.readDataNumber(princess, "y") * 16
-        attack.lifespan = 300
+        attack_effect.setPosition(mp.getPlayerSprite(mp.getPlayerByIndex(playerId)).x, mp.getPlayerSprite(mp.getPlayerByIndex(playerId)).y)
+        attack_effect.x += mp.getPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.x) * 16
+        attack_effect.y += mp.getPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.y) * 16
+        attack_effect.lifespan = 300
         animation.runImageAnimation(
-        attack,
-        [img`
+        attack_effect,
+        attack_animation,
+        100,
+        true
+        )
+        pause(250)
+    }
+}
+function dx (playerId: number) {
+    if (mp.isButtonPressed(mp.getPlayerByIndex(playerId), mp.MultiplayerButton.Left)) {
+        return -1
+    } else if (mp.isButtonPressed(mp.getPlayerByIndex(playerId), mp.MultiplayerButton.Right)) {
+        return 1
+    }
+    return 0
+}
+function dy (playerId: number) {
+    if (mp.isButtonPressed(mp.getPlayerByIndex(playerId), mp.MultiplayerButton.Up)) {
+        return -1
+    } else if (mp.isButtonPressed(mp.getPlayerByIndex(playerId), mp.MultiplayerButton.Down)) {
+        return 1
+    }
+    return 0
+}
+function smoothGridMove (playerId: number, x: number, y: number) {
+    if (mp.getPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.moving) == 0) {
+        if (x == mp.getPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.x) && y == mp.getPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.y)) {
+            mp.setPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.moving, 1)
+            mp.setPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.animation, 1)
+            mp.getPlayerSprite(mp.getPlayerByIndex(playerId)).setVelocity(x * 50, y * 50)
+            pause(300)
+            mp.setPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.moving, 0)
+            mp.getPlayerSprite(mp.getPlayerByIndex(playerId)).setVelocity(0, 0)
+            snapToGrid(mp.getPlayerSprite(mp.getPlayerByIndex(playerId)))
+        } else if (x != 0 || y != 0) {
+            mp.setPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.x, x)
+            mp.setPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.y, y)
+            pause(60)
+        } else if (mp.getPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.animation) == 1) {
+            mp.setPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.animation, 0)
+        }
+    }
+}
+mp.onButtonEvent(mp.MultiplayerButton.A, ControllerButtonEvent.Pressed, function (p) {
+    mp.setPlayerState(p, MultiplayerState.animation, 2)
+})
+function currentDir (playerId: number) {
+    return dirFromXY(mp.getPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.x), mp.getPlayerState(mp.getPlayerByIndex(playerId), MultiplayerState.y))
+}
+function yFromDir (dir: number) {
+    if (dir == 0) {
+        return 0
+    } else if (dir == 1) {
+        return -1
+    } else if (dir == 2) {
+        return 0
+    } else if (dir == 3) {
+        return 1
+    }
+    return 0
+}
+function snapToGrid (sprite: Sprite) {
+    sprite.x = Math.round((sprite.x - 8) / 16) * 16 + 8
+    sprite.y = Math.round((sprite.y - 8) / 16) * 16 + 8
+}
+function generateDungeon (seed: number, width: number, height: number, density: number) {
+    rng = Random.createRNG(seed)
+    tiles.setCurrentTilemap(tilemap`level12`)
+    for (let index = 0; index < density; index++) {
+        generateRoom(width, height)
+    }
+}
+let attack_effect: Sprite = null
+let rng: FastRandomBlocks = null
+let worker: Sprite = null
+let pid = 0
+let animations: Image[][][][] = []
+animations = [[[
+assets.animation`idleLeft`,
+assets.animation`idleUp`,
+assets.animation`idleRight`,
+assets.animation`idleDown`
+], [
+assets.animation`walkLeft`,
+assets.animation`walkUp`,
+assets.animation`walkRight`,
+assets.animation`walkDown`
+], [
+assets.animation`attackLeft`,
+assets.animation`attackUp`,
+assets.animation`attackRight`,
+assets.animation`attackDown`
+]], [[
+assets.animation`idleDownLeft`,
+assets.animation`idleUpRight`,
+assets.animation`idleDownRight`,
+assets.animation`idleDownLeft`
+], [
+assets.animation`walkDownLeft`,
+assets.animation`walkUpRight`,
+assets.animation`walkDownRight`,
+assets.animation`walkDownLeft`
+]]]
+pid = 0
+generateDungeon(1, 50, 50, 30)
+forever(function () {
+    if (mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)) != runtimeVariables.nullValue()) {
+        smoothGridMove(0, dx(0), dy(0))
+        attack(0, [img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . 9 . . . . 
             . . . 9 . . . . . . 9 1 9 . . . 
@@ -274,10 +260,33 @@ forever(function () {
             . . . . 9 1 9 . . . . . . . . . 
             . . . . . 9 . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
-            `],
-        100,
-        true
-        )
-        pause(250)
+            `])
+    }
+})
+forever(function () {
+    if (mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three)) != runtimeVariables.nullValue()) {
+        smoothGridMove(1, dx(1), dy(1))
+    }
+})
+forever(function () {
+    if (mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three)) != runtimeVariables.nullValue()) {
+        smoothGridMove(2, dx(2), dy(2))
+    }
+})
+forever(function () {
+    if (mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four)) != runtimeVariables.nullValue()) {
+        smoothGridMove(3, dx(3), dy(3))
+    }
+})
+game.onUpdate(function () {
+    for (let currentPlayer of mp.allPlayers()) {
+        if (currentAnimation(mp.getPlayerProperty(currentPlayer, mp.PlayerProperty.Index)) != mp.getPlayerState(currentPlayer, MultiplayerState.animation)) {
+            animation.runImageAnimation(
+            mp.getPlayerSprite(currentPlayer),
+            animations[mp.getPlayerProperty(currentPlayer, mp.PlayerProperty.Index)][mp.getPlayerState(currentPlayer, MultiplayerState.animation)][currentDir(mp.getPlayerProperty(currentPlayer, mp.PlayerProperty.Index))],
+            150,
+            true
+            )
+        }
     }
 })
